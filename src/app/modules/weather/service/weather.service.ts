55113edcfaeb3fb90a37614d401ddb3176f476b4
Ivan Passos <ivanfilho22@icloud.com>
2024-04-04 19:20:59 -0300

    Aula 12 - Criando serviço de previsão do tempo

---
+++ src/app/modules/weather/service/weather.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private API_KEY: string = "73adfadb5d44f7e802863627a6050016"
+
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  getWeatherData(cityName: string): Observable<any> {
+    return this._http.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}%unitis=metrics&mode=json&apiid=${this.API_KEY}`)
+  }
+}
