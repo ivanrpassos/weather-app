8f83065080f27e7d4a9a47e9aa52f0a8e2503104
Ivan Passos <ivanfilho22@icloud.com>
2024-04-08 17:53:12 -0300

    Aula 16 - Implementando componente Weather Card - Parte 1

--- src/app/modules/weather/components/weather-card/weather-card.component.ts
+++ src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -1,15 +1,16 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
 import { WeatherData } from 'src/app/models/types/wheater-data';
 
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
 })
-export class WeatherCardComponent implements OnInit{
+export class WeatherCardComponent {
   @Input() weatherDataInput!: WeatherData;
-
-  ngOnInit(): void {
-    console.log(this.weatherDataInput)
-  }
   
+  minTemperatureIcon = faTemperatureLow
+  maxTemperatureIcon = faTemperatureHigh
+  humidityIcon = faDroplet
+  windIcon = faWind
 }
